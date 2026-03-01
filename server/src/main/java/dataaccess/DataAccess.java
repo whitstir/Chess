661cf5ebab3e055a2c620cd3654cc7aceadbb096
Whitney <whitstirling@gmail.com>
2026-03-01 16:32:47 -0700

    Added DataAccess interface similar to PetShop

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,23 +1,28 @@
 package dataaccess;
 
-import model.AuthData;
-import model.GameData;
-import model.UserData;
+import model.*;
 
 import javax.xml.crypto.Data;
 import java.util.Collection;
 
 public interface DataAccess {
     void insertUser(UserData user) throws DataAccessException;
+
     UserData getUser(String username) throws DataAccessException;
 
-    void insertAuth(AuthData auth) throws DataAccessException;
+    void createAuth(AuthData auth) throws DataAccessException;
+
     AuthData getAuth(String authToken) throws DataAccessException;
+
     void deleteAuth(String authToken) throws DataAccessException;
 
-    int insertGame(GameData game) throws DataAccessException;
+    int createGame(GameData game) throws DataAccessException;
+
     GameData getGame(int gameID) throws DataAccessException;
+
     Collection<GameData> listGames() throws DataAccessException;
+
     void updateGame(GameData game) throws DataAccessException;
 
+    void clear() throws DataAccessException;
 }
---
+++ server/src/test/ServerTests.java
@@ -0,0 +1,2 @@
+public class ServerTests {
+}
