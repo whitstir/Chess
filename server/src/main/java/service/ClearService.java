01b6e3e8ca7cc9a54ad003d2e3b10abbcfc72075
Whitney <whitstirling@gmail.com>
2026-03-02 00:28:15 -0700

    Clear service and its test pass!

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,17 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
+public class ClearService {
+
+    private final DataAccess dao;
+
+    public ClearService(DataAccess dao) {
+        this.dao = dao;
+    }
+
+    public void clear() throws DataAccessException {
+        dao.clear();
+    }
+}
--- server/src/test/ServerTests.java
+++ server/src/test/ServerTests.java
@@ -1,2 +1,38 @@
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.ClearService;
+import service.UserService;
+
+import javax.xml.crypto.Data;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class ServerTests {
+
+    private MemoryDataAccess testDAO;
+    private UserData testUser;
+
+    @BeforeEach
+    public void setup() {
+        testDAO = new MemoryDataAccess();
+        testUser = new UserData("whitney", "12345", "whitstir@byu.edu");
+    }
+
+    @Test
+    public void clearData() throws DataAccessException {
+        ClearService testService = new ClearService(testDAO);
+        testDAO.createUser(testUser);
+        testService.clear();
+        assertNull(testDAO.getUser(testUser.username()));
+    }
+
+
 }
