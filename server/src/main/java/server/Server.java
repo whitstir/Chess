aa6329415e25a94c039025f890938ace00a0c5c0
Whitney <whitstirling@gmail.com>
2026-03-02 15:01:28 -0700

    Setting up the server like PetShop

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,17 +1,64 @@
 package server;
 
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
 import io.javalin.*;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
+import io.javalin.http.Context;
 
 public class Server {
 
     private final Javalin javalin;
+    private final DataAccess dao = new MemoryDataAccess();
+    private final ClearService clearService = new ClearService(dao);
+    private final UserService userService = new UserService(dao);
+    private final GameService gameService = new GameService(dao);
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .delete("/db", this::clear)
+                .post("/user", this::register)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::createGame)
+                .put("/game", this::joinGame);
+
+
 
         // Register your endpoints and exception handlers here.
 
 
+
+    }
+
+    private void clear(Context ctx) {
+
+    }
+
+    private void register(Context ctx) {
+
+    }
+
+    private void login(Context ctx) {
+
+    }
+
+    private void logout(Context ctx) {
+
+    }
+
+    private void listGames(Context ctx) {
+
+    }
+
+    private void createGame(Context ctx) {
+
+    }
+
+    private void joinGame(Context ctx) {
 
     }
 
