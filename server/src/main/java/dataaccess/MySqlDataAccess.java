fe4d1195a272dc28affa8072d7d81739e9a30be1
Whitney <whitstirling@gmail.com>
2026-03-09 21:54:46 -0600

    Setup MemoryDataAccess class like Petshop

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,50 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public class MySqlDataAccess implements DataAccess {
+    public void clear() {
+
+    }
+
+    public void createUser(UserData user) {
+
+    }
+
+    public UserData getUser(String username) {
+
+    }
+
+    public void createGame(GameData game) {
+
+    }
+
+    public GameData getGame(int gameID) {
+
+    }
+
+    public Collection<GameData> listGames() {
+
+    }
+
+    public void updateGame(GameData game) {
+
+    }
+
+    public void createAuth(AuthData auth) {
+
+    }
+
+    public AuthData getAuth(String authToken) {
+
+    }
+
+    public void deleteAuth(String authToken) {
+
+    }
+
+}
