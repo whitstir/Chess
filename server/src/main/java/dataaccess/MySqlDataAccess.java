221e61b851f933211940a85f4e29e82aa13d359f
Whitney <whitstirling@gmail.com>
2026-03-10 17:35:28 -0600

    Added some more petshop stuff for the MySQL class

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -4,9 +4,16 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 
 public class MySqlDataAccess implements DataAccess {
+
+    public MySqlDataAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
     public void clear() {
 
     }
@@ -45,6 +52,33 @@ public class MySqlDataAccess implements DataAccess {
 
     public void deleteAuth(String authToken) {
 
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  pet (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 
 }
---
+++ server/src/test/java/dataaccess/DataTests.java
@@ -0,0 +1,11 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class DataTests {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+}
