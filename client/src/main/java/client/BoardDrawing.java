8de16ede6e9fd7b608a67eb434616fa8e7885515
Whitney <whitstirling@gmail.com>
2026-03-27 13:50:51 -0600

    implement logic for the drawing

--- client/src/main/java/client/BoardDrawing.java
+++ client/src/main/java/client/BoardDrawing.java
@@ -1,6 +1,11 @@
 package client;
 
 import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import ui.EscapeSequences;
+
+import java.util.concurrent.ScheduledExecutorService;
 
 
 public class BoardDrawing {
@@ -25,7 +30,52 @@ public class BoardDrawing {
     }
 
     private void makeSquare(ChessGame game, int row, int col) {
+        ChessPosition position = new ChessPosition(row, col);
+        ChessPiece chessPiece = game.getBoard().getPiece(position);
+        colorSquare(row, col);
+        if (chessPiece == null) {
+            System.out.println(EscapeSequences.EMPTY);
+        } else {
+            System.out.println(getPiece(chessPiece));
+        }
+    }
 
+    private String getPiece(ChessPiece chessPiece) {
+        return switch (chessPiece.getTeamColor()) {
+            case WHITE -> getWhitePiece(chessPiece);
+            case BLACK -> getBlackPiece(chessPiece);
+        };
+    }
+
+    private String getBlackPiece(ChessPiece chessPiece) {
+        return switch (chessPiece.getPieceType()) {
+            case KING -> EscapeSequences.BLACK_KING;
+            case QUEEN -> EscapeSequences.BLACK_QUEEN;
+            case BISHOP -> EscapeSequences.BLACK_BISHOP;
+            case KNIGHT -> EscapeSequences.BLACK_KNIGHT;
+            case ROOK -> EscapeSequences.BLACK_ROOK;
+            case PAWN -> EscapeSequences.BLACK_PAWN;
+        };
+    }
+
+    private String getWhitePiece(ChessPiece chessPiece) {
+        return switch (chessPiece.getPieceType()) {
+            case KING -> EscapeSequences.WHITE_KING;
+            case QUEEN -> EscapeSequences.WHITE_QUEEN;
+            case BISHOP -> EscapeSequences.WHITE_BISHOP;
+            case KNIGHT -> EscapeSequences.WHITE_KNIGHT;
+            case ROOK -> EscapeSequences.WHITE_ROOK;
+            case PAWN -> EscapeSequences.WHITE_PAWN;
+        };
+    }
+
+    private void colorSquare(int row, int col) {
+        boolean lightSquare = ((row + col) % 2 == 0);
+        if (lightSquare) {
+            System.out.println(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+        } else {
+            System.out.println(EscapeSequences.SET_BG_COLOR_DARK_GREY);
+        }
     }
 
     private void drawBlackBoard(ChessGame game) {
