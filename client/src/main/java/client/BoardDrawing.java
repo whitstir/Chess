29409229078cb77abd10812f5aa917310669fce3
Whitney <whitstirling@gmail.com>
2026-03-27 11:45:09 -0600

    Made rough layout for the board drawing

--- client/src/main/java/client/BoardDrawing.java
+++ client/src/main/java/client/BoardDrawing.java
@@ -2,18 +2,48 @@ package client;
 
 import chess.ChessGame;
 
+
 public class BoardDrawing {
-    public static void drawBoard(ChessGame game, ChessGame.TeamColor side) {
+    public void drawBoard(ChessGame game, ChessGame.TeamColor side) {
         if (side == ChessGame.TeamColor.WHITE) {
             drawWhiteBoard(game);
         } else {
-            drawBlackBoarD(game);
+            drawBlackBoard(game);
         }
     }
 
-    private static void drawBlackBoarD(ChessGame game) {
+    private  void drawWhiteBoard(ChessGame game) {
+        printColumnsWhite();
+        for (int row = 8; row > 0; row--) {
+            System.out.print(row + " ");
+            for (int col = 1; col <= 8; col++) {
+                makeSquare(game, row, col);
+            }
+            System.out.println(row + " ");
+        }
+        printColumnsWhite();
     }
 
-    private static void drawWhiteBoard(ChessGame game) {
+    private void makeSquare(ChessGame game, int row, int col) {
+    }
+
+    private void drawBlackBoard(ChessGame game) {
+        printColumnsBlack();
+        for (int row = 1; row <= 8; row++) {
+            System.out.print(row + " ");
+            for (int col = 8; col > 0; col--) {
+                makeSquare(game, row, col);
+            }
+            System.out.println(row + " ");
+        }
+        printColumnsBlack();
+    }
+
+    private void printColumnsWhite() {
+
+    }
+
+    private void printColumnsBlack() {
+
     }
 }
