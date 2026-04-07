5a856835257f83634c4b7432984ce2ab8ac08c7a
Whitney <whitstirling@gmail.com>
2026-04-07 16:38:17 -0600

    Made the WebSocket Communicator based off of the websocket instruction page.

---
+++ client/src/main/java/repl/WebSocketCommunicator.java
@@ -0,0 +1,49 @@
+package repl;
+
+import com.google.gson.Gson;
+import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class WebSocketCommunicator extends Endpoint {
+    public Session session;
+    private final Gson gson = new Gson();
+
+    public WebSocketCommunicator(String serverUrl, ServerMessageObserver observer) throws Exception {
+        URI uri = new URI(serverUrl.replace("http", "ws") + "/ws");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        session = container.connectToServer(this, uri);
+        this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+            ServerMessage baseMessage = gson.fromJson(message, ServerMessage.class);
+            ServerMessage typed = null;
+            if (baseMessage.getServerMessageType() == ServerMessage.ServerMessageType.LOAD_GAME) {
+                typed = gson.fromJson(message, LoadGameMessage.class);
+            } else if (baseMessage.getServerMessageType() == ServerMessage.ServerMessageType.ERROR) {
+                typed = gson.fromJson(message, ErrorMessage.class);
+            } else if (baseMessage.getServerMessageType() == ServerMessage.ServerMessageType.NOTIFICATION) {
+                typed = gson.fromJson(message, NotificationMessage.class);
+            }
+            observer.onMessage(typed);
+        });
+    }
+
+    public void send(UserGameCommand command) throws IOException {
+        session.getBasicRemote().sendText(gson.toJson(command));
+    }
+
+    public void close() throws IOException {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+}
