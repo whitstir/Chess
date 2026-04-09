42927da87115dfae04134eb68a4df8de0ad7e184
Whitney <whitstirling@gmail.com>
2026-04-09 10:54:43 -0600

    Fixing code quality now?

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -13,7 +13,9 @@ public class ConnectionManager {
 
     public void remove(int gameID, WsContext session) {
         Set<WsContext> sessions = connections.get(gameID);
-        if (sessions != null) sessions.remove(session);
+        if (sessions != null) {
+            sessions.remove(session);
+        }
     }
 
     public void removeFromAll(WsContext session) {
@@ -23,13 +25,17 @@ public class ConnectionManager {
     public void broadcast(int gameID, WsContext exclude, String message) {
         Set<WsContext> sessions = connections.getOrDefault(gameID, Set.of());
         for (WsContext s : sessions) {
-            if (s.session.isOpen() && !s.equals(exclude)) s.send(message);
+            if (s.session.isOpen() && !s.equals(exclude)) {
+                s.send(message);
+            }
         }
     }
 
     public void broadcastAll(int gameID, String message) {
         for (WsContext s : connections.getOrDefault(gameID, Set.of())) {
-            if (s.session.isOpen()) s.send(message);
+            if (s.session.isOpen()) {
+                s.send(message);
+            }
         }
     }
 }
